a68f1e3380dff8beaadad50e6fc7851379d145e1
BUDGamers <BUDgamers@gmail.com>
2014-05-15 15:59:36 +0100

    Initial Commit

---
+++ src/main/java/org/jimbo/lock/LockContainerMod.java
@@ -0,0 +1,13 @@
+package org.jimbo.lock;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "lockcontainers", name = "Lock Containers", version = "1.0")
+public class LockContainerMod {
+	
+	public static final String MODID = "lockcontainers";
+	public static final String NAME = "Lock Containers";
+	public static final String VERSION = "1.0";
+	
+	
+}
