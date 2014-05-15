d498415e35efbd7fc3469b13b1280d8c5a5535e3
BUDGamers <BUDgamers@gmail.com>
2014-05-15 16:02:30 +0100

    Added an event handler
    Just the PreInit method

--- src/main/java/org/jimbo/lock/LockContainerMod.java
+++ src/main/java/org/jimbo/lock/LockContainerMod.java
@@ -1,6 +1,9 @@
 package org.jimbo.lock;
 
+import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "lockcontainers", name = "Lock Containers", version = "1.0")
 public class LockContainerMod {
@@ -9,5 +12,16 @@ public class LockContainerMod {
 	public static final String NAME = "Lock Containers";
 	public static final String VERSION = "1.0";
 	
+	public boolean enabled = true;
 	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		Configuration config = new Configuration(e.getSuggestedConfigurationFile());
+		
+		config.load();
+		
+		enabled = config.get(Configuration.CATEGORY_GENERAL, "EnableMod", true).getBoolean(true);
+		
+		config.save();
+	}
 }
